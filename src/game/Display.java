025c38acb2e776128670edc34358d44e60387669
Megalunchbox <megalunchbox@gmail.com>
2017-08-31 12:32:20 -0500

    update width and height.
    final int width, height; and changed to capitals for consistency

--- src/game/Display.java
+++ src/game/Display.java
@@ -8,17 +8,17 @@ import javax.swing.JFrame;
 public class Display {
 
 	private JFrame display;
-	int width, height;
+	final int width, height;
 	public String title;
 	private Canvas displaycanvas;
 	
 	
 	
 	
-	public Display(String title, int width, int height) {
+	public Display(String title, int WIDTH, int HEIGHT) {
 		
-		this.width = width;
-		this.height = height;
+		this.WIDTH = WIDTH;
+		this.HEIGHT = HEIGHT;
 		this.title = title;
 		createDisplay();
 		
@@ -27,15 +27,15 @@ public class Display {
 	private void createDisplay() {
 		
 		display = new JFrame(title);
-		display.setSize(width, height);
+		display.setSize(WIDTH, HEIGHT);
 		display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		display.setResizable(false);
 		display.setLocationRelativeTo(null);
 		display.setVisible(true);
 		displaycanvas  = new Canvas();
-		displaycanvas.setPreferredSize(new Dimension(width,height));
-		displaycanvas.setMinimumSize(new Dimension(width,height));
-		displaycanvas.setMaximumSize(new Dimension(width,height));
+		displaycanvas.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		displaycanvas.setMinimumSize(new Dimension(WIDTH,HEIGHT));
+		displaycanvas.setMaximumSize(new Dimension(WIDTH,HEIGHT));
 		
 		display.add(displaycanvas);
 		display.pack();
