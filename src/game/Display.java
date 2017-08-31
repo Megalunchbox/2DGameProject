b782097fc3299bfc10be30221c6776feba8ea5ed
Megalunchbox <megalunchbox@gmail.com>
2017-08-30 19:27:14 -0500

    add display
    adds the Jframe with the display on it, for now its just a blank jframe
    lol

--- src/game/Display.java
+++ src/game/Display.java
@@ -1,5 +1,30 @@
 package game;
 
+import javax.swing.JFrame;
+
 public class Display {
 
+	private JFrame display;
+	int width, height;
+	String title;
+	
+	public Display(String title, int width, int height) {
+		
+		this.width = width;
+		this.height = height;
+		
+		createDisplay();
+		
+	}
+	
+	private void createDisplay() {
+		
+		display = new JFrame();
+		display.setSize(width, height);
+		display.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		display.setResizable(false);
+		display.setLocationRelativeTo(null);
+		
+	}
+	
 }
