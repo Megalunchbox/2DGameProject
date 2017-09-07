71c627189bd4d5c6123c53e14868b6fac299348b
Megalunchbox <megalunchbox@gmail.com>
2017-09-07 12:36:07 -0500

    remove un-needed comment

--- src/launcher/Launcher.java
+++ src/launcher/Launcher.java
@@ -1,10 +1,6 @@
 package launcher;
 import game.Thread.GameThread;
 
-
-// Well im now getting used to IntelliJ and I finally sucessfuly built this class. I had to rename all the files bcuz
-// i didn't know reformat code function existed.... fckme..
-
 public class Launcher {
 
 
